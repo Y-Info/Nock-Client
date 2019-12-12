e0d53b28aa9c248e42ce8b0bf53b76c5006572d7
Demarche <anais.demarche1@gmail.com>
2019-12-12 16:48:53 +0100

    create new pages

---
+++ src/views/Connect.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Vous allez rejoindre la room</h1>
+    <router-link to="/validate-adress">Retour</router-link>
+    <router-link to="/inscription">Confirmer</router-link>
+  </div>
+</template>
