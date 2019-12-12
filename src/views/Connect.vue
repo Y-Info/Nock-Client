7cb9913db605f94eb09e3065ba5c0a863186d0e7
Demarche <anais.demarche1@gmail.com>
2019-12-12 17:55:55 +0100

    create input form

--- src/views/Connect.vue
+++ src/views/Connect.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
     <h1>Vous allez rejoindre la room</h1>
-    <router-link to="/validate-adress">Retour</router-link>
-    <router-link to="/inscription">Confirmer</router-link>
+
+    <form id="connect" @submit="checkForm" action="/" method="post">
+      <p>
+        <label for="email">Email</label>
+          <input type="email" name="email" id="email" v-model="email">
+
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password" v-model="password" min="0" />
+      </p>
+    </form>
+
+    <router-link to="/">Mot de passe oublié ?</router-link>
+
+    <router-link class="boutonBefore" to="/validate-adress">Retour</router-link>
+    <router-link class="boutonAfter" to="/inscription">Confirmer</router-link>
   </div>
 </template>
