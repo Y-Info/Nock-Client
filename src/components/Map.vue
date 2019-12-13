b024d20e9f4a8af5881bf03705a7a3b2bdecbe57
ImMyst <titouan.galvani@laposte.net>
2019-12-13 12:18:53 +0100

    🔥 Add map and finish homepage design

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,5 +1,5 @@
 <template>
-  <l-map ref="map"> </l-map>
+  <l-map style="width:100%;height:250px" ref="map"></l-map>
 </template>
 
 <script>
@@ -8,6 +8,11 @@ import { LMap } from "vue2-leaflet";
 export default {
   components: {
     LMap
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.$refs.map.mapObject.setView([45.745670, 4.837680], 7);
+    })
   }
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,40 +1,49 @@
 <template>
-  <div class="container background h-100">
-    <div class="flex justify-between align-center">
-      <img src="../assets/icons/logo.svg" alt="Nock logo" />
-      <router-link to="/">
-        <img src="../assets/icons/profile-icon.svg" alt="Profile icon" />
-      </router-link>
-    </div>
-    <div class="images">
-      <img src="../assets/img/home-1.png" alt="Image loader-1" />
-      <div class="second-row">
-        <img src="../assets/img/home-2.png" alt="Image loader-2" />
-        <img src="../assets/img/home-3.png" alt="Image loader-3" />
-      </div>
-    </div>
-    <div class="mt-8">
-      <h2 class="uppercase text--yellow title">
-        Titre principal
-      </h2>
-      <p class="text">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut ero labore et dolore.
-      </p>
-      <div class="button-container">
-        <router-link
-          class="block bg--blue uppercase pa-4 border-radius-full button"
-          to="/"
-        >
-          Créer sa salle
+  <div class="h-100">
+    <div class="container background">
+      <div class="flex justify-between align-center">
+        <img src="../assets/icons/logo.svg" alt="Nock logo" />
+        <router-link to="/">
+          <img src="../assets/icons/profile-icon.svg" alt="Profile icon" />
         </router-link>
       </div>
-      <div class="mt-20">
-        <h2 class="uppercase text--yellow title">
-          Sous-titre
-        </h2>
-        <Map />
+      <div class="images">
+        <img src="../assets/img/home-1.png" alt="Image loader-1" />
+        <div class="second-row">
+          <img src="../assets/img/home-2.png" alt="Image loader-2" />
+          <img src="../assets/img/home-3.png" alt="Image loader-3" />
+        </div>
       </div>
+      <div class="mt-8">
+        <h2 class="uppercase text--yellow title">
+          Titre principal
+        </h2>
+        <p class="text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut ero labore et dolore.
+        </p>
+        <div class="button-container">
+          <router-link
+            class="block bg--blue uppercase pa-4 border-radius-full button"
+            to="/"
+          >
+            Créer sa salle
+          </router-link>
+        </div>
+        <div class="mt-20">
+          <h2 class="uppercase text--yellow title">
+            Sous-titre
+          </h2>
+          <Map />
+          <p class="text mt-20">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut ero labore et dolore.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div style="height:300px;margin-top:62px;" class="bg--yellow">
+      Footer
     </div>
   </div>
 </template>
