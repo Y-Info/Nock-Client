13a0e6e437a7ea16c79471f554ef3a7b6258ed49
Demarche <anais.demarche1@gmail.com>
2019-12-13 12:45:48 +0100

    final connect page

---
+++ src/components/menuConnect.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img class="arrow" src="../assets/icons/arrow.svg" alt="flèche retour">
+        <img class="logo" src="../assets/logo.png" alt="Nock logo">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .logo {
+        position: relative;
+        display: block;
+        margin: 20px auto;
+        z-index: 0;
+    }
+    .arrow{
+        position: fixed;
+        width: 20px;
+        height: 20px;
+        top: 40px;
+        left: 20px;
+    }
+</style>
