3e6e97c86ded807c10caa5ddf95bc06f34859bdd
Demarche <anais.demarche1@gmail.com>
2019-12-13 17:09:17 +0100

    create pag menu link
    create page : feed, alert, calendar and general info for the bottom menu

---
+++ src/components/menuBottom.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="menuBottom">
+        <router-link to="/feed">
+            <img class="feed" src="../assets/icons/feedIcon.svg" alt="lien feed" />
+        </router-link>
+        <router-link to="/alert">
+            <img class="alert" src="../assets/icons/alerteIcon.svg" alt="lien alerte" />
+        </router-link>
+        <router-link to="/calendar">
+            <img class="calendar" src="../assets/icons/calendarIcon.svg" alt="lien calendrier" />
+        </router-link>
+        <router-link to="/info">
+            <img class="info" src="../assets/icons/informationIcon.svg" alt="lien informations générales" />
+        </router-link>
+    </div>
+</template>
+
--- src/router/index.js
+++ src/router/index.js
@@ -53,6 +53,42 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Inscription.vue")
+  },
+  {
+    path: "/feed",
+    name: "feed",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import(/* webpackChunkName: "about" */ "../views/feed.vue")
+  },
+  {
+    path: "/alert",
+    name: "alert",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import(/* webpackChunkName: "about" */ "../views/alert.vue")
+  },
+  {
+    path: "/calendar",
+    name: "calendar",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import(/* webpackChunkName: "about" */ "../views/calendar.vue")
+  },
+  {
+    path: "/info",
+    name: "info",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import(/* webpackChunkName: "about" */ "../views/info.vue")
   }
 ];
 
