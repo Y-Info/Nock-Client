685f46b02e70221c9ee44ffc76cb8e684aa30c1a
Demarche <anais.demarche1@gmail.com>
2019-12-20 12:15:08 +0100

    final page profil

--- src/views/inscription.vue
+++ src/views/inscription.vue
@@ -1,150 +1,152 @@
 <template>
-  <div>
-    <menu-connect route="/connect" />
+    <div>
+        <menu-connect route="/connect"/>
 
-    <h2>Création du compte</h2>
+        <h2>Création du compte</h2>
 
-    <form id="connect" @submit="checkForm" action="/" method="post">
-      <div class="field-wrap">
-        <label for="name"
-          ><img src="../assets/icons/connectPeople.svg" alt="icon people"
-        /></label>
-        <input class="name" type="text" name="name" id="name" v-model="name" />
-      </div>
-      <div class="field-wrap">
-        <label for="email"
-          ><img src="../assets/icons/mailIcon.svg" alt="icon mail"
-        /></label>
-        <input
-          class="email"
-          type="email"
-          name="email"
-          id="email"
-          v-model="email"
-          min="0"
-        />
-      </div>
-      <div class="field-wrap">
-        <label for="password"
-          ><img src="../assets/icons/lock-icon.svg" alt="icon mail"
-        /></label>
-        <input
-          class="password"
-          type="password"
-          name="password"
-          id="password"
-          v-model="password"
-          min="0"
-        />
-      </div>
-      <div class="field-wrap">
-        <label for="confirmPassword"
-          ><img src="../assets/icons/lock-icon.svg" alt="icon mail"
-        /></label>
-        <input
-          class="password"
-          type="password"
-          name="password"
-          id="confirmPassword"
-          v-model="password"
-          min="0"
-        />
-      </div>
-    </form>
+        <form id="connect" @submit="checkForm" action="/" method="post">
+            <div class="field-wrap">
+                <label for="name"
+                ><img src="../assets/icons/connectPeople.svg" alt="icon people"
+                /></label>
+                <input class="name" type="text" name="name" id="name" v-model="name"/>
+            </div>
+            <div class="field-wrap">
+                <label for="email"
+                ><img src="../assets/icons/mailIcon.svg" alt="icon mail"
+                /></label>
+                <input
+                        class="email"
+                        type="email"
+                        name="email"
+                        id="email"
+                        v-model="email"
+                        min="0"
+                />
+            </div>
+            <div class="field-wrap">
+                <label for="password"
+                ><img src="../assets/icons/lock-icon.svg" alt="icon mail"
+                /></label>
+                <input
+                        class="password"
+                        type="password"
+                        name="password"
+                        id="password"
+                        v-model="password"
+                        min="0"
+                />
+            </div>
+            <div class="field-wrap">
+                <label for="confirmPassword"
+                ><img src="../assets/icons/lock-icon.svg" alt="icon mail"
+                /></label>
+                <input
+                        class="password"
+                        type="password"
+                        name="password"
+                        id="confirmPassword"
+                        v-model="password"
+                        min="0"
+                />
+            </div>
+        </form>
 
-    <div class="button-container">
-      <router-link class="buttonClick buttonBefore" to="/validate-adress"
-        >Confirmation</router-link
-      >
+        <div class="button-container">
+            <router-link class="buttonClick buttonBefore" to="/validate-adress"
+            >Confirmation
+            </router-link
+            >
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import menuConnect from "../components/menu-connect";
+    import menuConnect from "../components/menu-connect";
 
-export default {
-  components: {
-    menuConnect
-  }
-};
+    export default {
+        components: {
+            menuConnect
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
-h2 {
-  margin-top: 70px;
-  font-size: 14px;
-  text-transform: uppercase;
-  color: #ffba00;
-  margin-left: 20px;
-}
-.button-container {
-  font-weight: bold;
-  text-align: center;
-  margin-top: 70px;
-  font-size: 10px;
-  text-transform: uppercase;
-
-  .buttonClick {
-    display: block;
-    margin: 20px 10px 10px 10px;
-    padding: 18px 40px;
-    border-radius: 5px;
-    text-decoration: none;
-
-    -webkit-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
-    -moz-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
-    box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
-  }
-
-  .buttonBefore {
-    background-color: #2e3460;
-    color: white;
-  }
-}
-
-form {
-  margin-top: 30px;
-  display: block;
-  position: relative;
-
-  .mdp {
-    position: absolute;
-    bottom: -30px;
-    right: 20px;
-    font-size: 10px;
-    text-decoration: none;
-    color: #ffba00;
-    font-family: Montserrat B, sans-serif;
-  }
-
-  .field-wrap {
-    position: relative;
-    margin-bottom: 40px;
-    text-align: center;
-
-    input {
-      margin: 0 auto;
-      border: none;
-      border-bottom: #2e3460 1px solid;
-      width: 90%;
-      padding: 0 0 15px 45px;
-      font-size: 14px;
-      font-family: Montserrat B, sans-serif;
-      color: #2e3460;
+    h2 {
+        margin-top: 70px;
+        font-size: 14px;
+        text-transform: uppercase;
+        color: #ffba00;
+        margin-left: 20px;
     }
 
-    label {
-      position: absolute;
-      transform: translateY(6px);
-      left: 20px;
-      top: -10px;
-      color: rgba($white, 0.5);
-      transition: all 0.25s ease;
-      -webkit-backface-visibility: hidden;
-      pointer-events: none;
-      font-size: 22px;
+    .button-container {
+        font-weight: bold;
+        text-align: center;
+        margin-top: 70px;
+        font-size: 10px;
+        text-transform: uppercase;
+
+        .buttonClick {
+            display: block;
+            margin: 20px 10px 10px 10px;
+            padding: 18px 40px;
+            border-radius: 5px;
+            text-decoration: none;
+
+            -webkit-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+            -moz-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+            box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+        }
+
+        .buttonBefore {
+            background-color: #2e3460;
+            color: white;
+        }
     }
-  }
-}
+
+    form {
+        margin-top: 30px;
+        display: block;
+        position: relative;
+
+        .mdp {
+            position: absolute;
+            bottom: -30px;
+            right: 20px;
+            font-size: 10px;
+            text-decoration: none;
+            color: #ffba00;
+            font-family: Montserrat B, sans-serif;
+        }
+
+        .field-wrap {
+            position: relative;
+            margin-bottom: 40px;
+            text-align: center;
+
+            input {
+                margin: 0 auto;
+                border: none;
+                border-bottom: #2e3460 1px solid;
+                width: 90%;
+                padding: 0 0 15px 45px;
+                font-size: 14px;
+                font-family: Montserrat B, sans-serif;
+                color: #2e3460;
+            }
+
+            label {
+                position: absolute;
+                transform: translateY(6px);
+                left: 20px;
+                top: -10px;
+                color: rgba($white, 0.5);
+                transition: all 0.25s ease;
+                -webkit-backface-visibility: hidden;
+                pointer-events: none;
+                font-size: 22px;
+            }
+        }
+    }
 </style>
---
+++ src/views/profile.vue
@@ -0,0 +1,135 @@
+<template>
+    <div>
+        <menu-connect route="/connect"/>
+        <div class="profilUser">
+            <img src="../assets/img/profilUser.svg" alt="photo de profil">
+            <p class="name">Nom Prénom</p>
+        </div>
+        <div class="infoUser">
+            <div class="info">
+                <img src="../assets/icons/phone.svg" alt="icon phone">
+                <p>+33 6 78 96 84 37</p>
+                <div class="ligne"></div>
+            </div>
+            <div class="info">
+                <img src="../assets/icons/mailIcon.svg" alt="icon mail">
+                <p>toto@gmail.com</p>
+                <div class="ligne"></div>
+            </div>
+            <div class="info">
+                <img src="../assets/icons/mailIcon.svg" alt="icon mail">
+                <p>toto@gmail.com</p>
+            </div>
+        </div>
+        <div class="button-container">
+            <router-link class="buttonClick buttonBefore" to="">Modifier les informations</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    import menuConnect from "../components/menu-connect";
+
+    export default {
+        components: {
+            menuConnect
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .profilUser {
+        background-color: #FFFFFF;
+        padding: 20px;
+        width: 90%;
+        height: 210px;
+        margin: 0 auto;
+        border-radius: 5px;
+
+        -webkit-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+        -moz-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+        box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+
+        img {
+            display: inline-block;
+            vertical-align: top;
+            box-sizing: border-box;
+            width: 40px;
+            height: 40px;
+        }
+
+        .name {
+            display: inline-block;
+            vertical-align: top;
+            box-sizing: border-box;
+            width: 40%;
+            margin: 10px 20px;
+            font-size: 14px;
+            font-weight: 600;
+            color: #2E3460;
+        }
+    }
+
+    .infoUser {
+        background-color: #FFFFFF;
+        padding: 20px;
+        width: 90%;
+        height: 210px;
+        margin: 20px auto;
+        border-radius: 5px;
+        font-size: 12px;
+
+        -webkit-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+        -moz-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+        box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+
+        .info {
+            margin-top: 20px;
+
+            img {
+                display: inline-block;
+                vertical-align: top;
+                box-sizing: border-box;
+                width: 5%;
+            }
+
+            p {
+                display: inline-block;
+                vertical-align: top;
+                box-sizing: border-box;
+                margin: 0 0 0 45px;
+            }
+
+            .ligne {
+                background-color: #F8F8F8;
+                width: 100%;
+                height: 2px;
+                margin-top: 20px;
+            }
+        }
+    }
+    .button-container {
+        font-weight: bold;
+        text-align: center;
+        margin-top: 70px;
+        font-size: 10px;
+        text-transform: uppercase;
+
+        .buttonClick {
+            display: block;
+            margin: 20px 10px 10px 10px;
+            padding: 18px 40px;
+            border-radius: 5px;
+            text-decoration: none;
+
+            -webkit-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+            -moz-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+            box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+        }
+
+        .buttonBefore {
+            background-color: #2e3460;
+            color: white;
+        }
+    }
+</style>
