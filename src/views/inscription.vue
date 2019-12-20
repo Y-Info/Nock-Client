1480faad7bb13b12f9af6480a3165c6efe05f201
Demarche <anais.demarche1@gmail.com>
2019-12-20 14:53:56 +0100

    correction inscription

--- src/views/inscription.vue
+++ src/views/inscription.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <menu-connect route="/connect"/>
+        <menu-connect route="/connect" />
 
         <h2>Création du compte</h2>
 
@@ -93,19 +93,52 @@
     import menuConnect from "../components/menu-connect";
     import axios from "axios";
 
-export default {
-  components: {
-    menuConnect
-  },
-  data() {
-    return {
-      user: {
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        verifPassword: ""
-      }
+    export default {
+        components: {
+            menuConnect
+        },
+        data() {
+            return {
+                user: {
+                    firstName: "",
+                    lastName: "",
+                    email: "",
+                    password: "",
+                    verifPassword: ""
+                }
+            };
+        },
+        methods: {
+            sendForm() {
+                if (this.password === this.verifPassword) {
+                    if (this.firstName !== "" || this.lastName !== "") {
+                        if (this.email !== "") {
+                            axios
+                                .post(`https://nock-nock.herokuapp.com/api/auth/signup`, {
+                                    firstName: this.user.firstName,
+                                    lastName: this.user.lastName,
+                                    email: this.user.email,
+                                    password: this.user.password
+                                })
+                                .then(() => {
+                                    this.$router.push("/connect");
+                                    this.$toasted.show("Compte créer", {
+                                        theme: "toasted-primary",
+                                        position: "top-right",
+                                        duration: 5000
+                                    });
+                                });
+                        } else {
+                            this.emailError = !this.emailError;
+                        }
+                    } else {
+                        this.namesError = !this.namesError;
+                    }
+                } else {
+                    this.passwordError = !this.passwordError;
+                }
+            }
+        }
     };
 </script>
 
@@ -117,7 +150,6 @@ export default {
         color: #ffba00;
         margin-left: 20px;
     }
-
     .button-container {
         font-weight: bold;
         text-align: center;
@@ -131,97 +163,6 @@ export default {
             padding: 18px 40px;
             border-radius: 5px;
             text-transform: uppercase;
-            text-decoration: none;
-
-            .buttonClick {
-                display: block;
-                margin: 20px 10px 10px 10px;
-                padding: 18px 40px;
-                border-radius: 5px;
-                text-decoration: none;
-
-                -webkit-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
-                -moz-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
-                box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
-            }
-
-            .buttonBefore {
-                background-color: #2e3460;
-                color: white;
-            }
-        }
-
-        form {
-            margin-top: 30px;
-            display: block;
-            position: relative;
-
-            .mdp {
-                position: absolute;
-                bottom: -30px;
-                right: 20px;
-                font-size: 10px;
-                text-decoration: none;
-                color: #ffba00;
-                font-family: Montserrat B, sans-serif;
-            }
-
-            .field-wrap {
-                position: relative;
-                margin-bottom: 40px;
-                text-align: center;
-
-                input {
-                    margin: 0 auto;
-                    border: none;
-                    border-bottom: #2e3460 1px solid;
-                    width: 90%;
-                    padding: 0 0 15px 45px;
-                    font-size: 14px;
-                    font-family: Montserrat B, sans-serif;
-                    color: #2e3460;
-                }
-
-                label {
-                    position: absolute;
-                    transform: translateY(6px);
-                    left: 20px;
-                    top: -10px;
-                    color: rgba($white, 0.5);
-                    transition: all 0.25s ease;
-                    -webkit-backface-visibility: hidden;
-                    pointer-events: none;
-                    font-size: 22px;
-                }
-            }
-        }
-    }
-
-    .errors {
-        font-size: 12px;
-        color: crimson;
-    }
-
-    h2 {
-        margin-top: 70px;
-        font-size: 14px;
-        text-transform: uppercase;
-        color: #ffba00;
-        margin-left: 20px;
-    }
-
-    .button-container {
-        font-weight: bold;
-        text-align: center;
-        margin-top: 70px;
-        font-size: 10px;
-        text-transform: uppercase;
-
-        .buttonClick {
-            display: block;
-            margin: 20px 10px 10px 10px;
-            padding: 18px 40px;
-            border-radius: 5px;
             text-decoration: none;
 
             -webkit-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
@@ -278,7 +219,9 @@ export default {
                 font-size: 22px;
             }
         }
+        .errors {
+            font-size: 12px;
+            color: crimson;
+        }
     }
-
-
 </style>
