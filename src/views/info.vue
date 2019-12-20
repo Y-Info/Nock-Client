db2584b369a1a8bc600be78c02deff937be9b527
Demarche <anais.demarche1@gmail.com>
2019-12-20 10:17:06 +0100

    Page General information conception

--- src/views/info.vue
+++ src/views/info.vue
@@ -2,6 +2,27 @@
     <div>
         <menuRight/>
         <menu-bottom/>
+        <div class="content">
+            <h3>Titre du l'infos</h3>
+            <img src="../assets/img/trash.jpg" alt="photo poubelle">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet debitis eum odit quisquam quod sint,
+                vel veniam. Assumenda deserunt eius est, exercitationem facilis ipsa nihil quasi soluta vero
+                voluptatum?</p>
+        </div>
+        <div class="content">
+            <h3>Titre du l'infos</h3>
+            <img src="../assets/img/trash.jpg" alt="photo poubelle">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet debitis eum odit quisquam quod sint,
+                vel veniam. Assumenda deserunt eius est, exercitationem facilis ipsa nihil quasi soluta vero
+                voluptatum?</p>
+        </div>
+        <div class="content">
+            <h3>Titre du l'infos</h3>
+            <img src="../assets/img/trash.jpg" alt="photo poubelle">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet debitis eum odit quisquam quod sint,
+                vel veniam. Assumenda deserunt eius est, exercitationem facilis ipsa nihil quasi soluta vero
+                voluptatum?</p>
+        </div>
     </div>
 </template>
 
@@ -18,5 +39,28 @@
 </script>
 
 <style lang="scss" scoped>
+    .content{
+        width: 90%;
+        background-color: #FFFFFF;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 30px;
+        padding: 20px;
+
+        -webkit-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+        -moz-box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+        box-shadow: 0 3px 6px -2px rgba(0, 0, 0, 0.5);
+
+            h3{
+                font-size: 14px;
+                color: #2E3460;
+            }
+            p{
+                font-size: 12px;
+            }
+            img{
+                width: 100%;
+            }
+    }
 
 </style>
