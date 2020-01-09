a6b1f2f0c79d1adc00eb5787b1aceb3c3085c0df
Demarche <anais.demarche1@gmail.com>
2020-01-09 10:23:30 +0100

    Begin concept menu

--- src/components/menu-right.vue
+++ src/components/menu-right.vue
@@ -1,31 +1,38 @@
 <template>
-  <div class="menu-right">
-    <h2>{{ title }}</h2>
-    <router-link to="/#">
-      <img class="menuRight" src="../assets/icons/menu.svg" alt="lien menu" />
-    </router-link>
-  </div>
+    <div class="menu-right">
+        <h2>{{ title }}</h2>
+        <div class="menu active">
+            <img class="menuRight" src="../assets/icons/menu.svg" alt="lien menu"/>
+
+
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  props: {
-    title: String
-  }
-};
+    export default {
+        props: {
+            title: String
+        }
+    };
 </script>
 
 <style lang="scss" scoped>
-h2 {
-  color: #ffba00;
-  font-size: 14px;
-  margin: 30px 0 0 30px;
-}
+    h2 {
+        color: #ffba00;
+        font-size: 14px;
+        margin: 30px 0 0 30px;
+    }
 
-.menuRight {
-  z-index: 30;
-  position: fixed;
-  right: 30px;
-  top: 30px;
-}
+    .menuRight {
+        z-index: 30;
+        position: fixed;
+        right: 30px;
+        top: 30px;
+    }
+
+    .active {
+      background-color: #FFBA00;
+    }
+
 </style>
